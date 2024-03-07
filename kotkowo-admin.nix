6eab6baed69c817012978ebc3ab2ae17908ee670
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-03-07 23:17:07 +0100

    ⬆️ Add update timers

--- kotkowo-admin.nix
+++ kotkowo-admin.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   services.caddy.virtualHosts."kotkowo-admin.ravensiris.xyz".extraConfig = ''
     reverse_proxy :1337
   '';
@@ -7,5 +7,26 @@
     ports = ["1337:1337"];
     volumes = ["/var/run/postgresql:/var/run/postgresql"];
     environmentFiles = ["/run/keys/strapi.env.secret"];
+  };
+
+  systemd.timers."kotkowo-admin-update" = {
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      Unit = "kotkowo-admin-update.service";
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+  };
+
+  systemd.services."kotkowo-admin-update" = {
+    script = ''
+      set -eu
+      ${pkgs.podman}/bin/podman pull ghcr.io/kotkowo/kotkowo-admin:master
+      ${pkgs.systemd}/bin/systemctl restart podman-kotkowo-admin
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
   };
 }
--- kotkowo.nix
+++ kotkowo.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   services.caddy.virtualHosts."kotkowo.ravensiris.xyz".extraConfig = ''
     reverse_proxy :4000
   '';
@@ -6,5 +6,26 @@
     image = "ghcr.io/kotkowo/kotkowo:latest";
     ports = ["4000:4000"];
     environmentFiles = ["/run/keys/kotkowo.env.secret"];
+  };
+
+  systemd.timers."kotkowo-update" = {
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      Unit = "kotkowo-update.service";
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+  };
+
+  systemd.services."kotkowo-update" = {
+    script = ''
+      set -eu
+      ${pkgs.podman}/bin/podman pull ghcr.io/kotkowo/kotkowo:latest
+      ${pkgs.systemd}/bin/systemctl restart podman-kotkowo
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
   };
 }
